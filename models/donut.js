10e5d3da3efdcc61464a08ac9603eb97b25b4948
maramdeepak <marammaharajulla@gmail.com>
2022-11-29 22:16:48 -0600

    Adde login page

--- models/donut.js
+++ models/donut.js
@@ -1,9 +1,8 @@
 const mongoose = require("mongoose") 
 const donutSchema = mongoose.Schema({ 
- donut_Name: String, 
- shop: String, 
- price: Number 
+    donut_Name: {type: String,required: [true, 'donut Name cannot be empty']},
+    shop: {type: String,required: [true, ' donut shop name cannot be empty']},
+    price: {type: Number,required: [true, 'dount price cannot be empty']}
 }) 
  
-module.exports = mongoose.model("Donut", 
-donutSchema) 
+module.exports = mongoose.model("Donut", donutSchema) 
--- routes/donuts.js
+++ routes/donuts.js
@@ -1,15 +1,28 @@
             var express = require('express'); 
             const donut_controlers= require('../controllers/donut'); 
             var router = express.Router(); 
+
+            // A little function to check if we have an authorized user and continue on 
+            //or 
+            // redirect to login. 
+            const secured = (req, res, next) => { 
+                if (req.user){ 
+                return next(); 
+                } 
+                req.session.returnTo = req.originalUrl; 
+                res.redirect("/login"); 
+            } 
+
+ 
             
             /* GET donuts */ 
             router.get('/', donut_controlers.donut_view_all_Page ); 
             /* GET detail costume page */ 
             router.get('/detail', donut_controlers.donut_view_one_Page); 
             /* GET create costume page */ 
-            router.get('/create', donut_controlers.donut_create_Page); 
+            router.get('/create',secured, donut_controlers.donut_create_Page); 
             /* GET create update page */ 
-            router.get('/update', donut_controlers.donut_update_Page); 
+            router.get('/update',secured, donut_controlers.donut_update_Page); 
             /* GET delete costume page */ 
-            router.get('/delete', donut_controlers.donut_delete_Page); 
+            router.get('/delete',secured, donut_controlers.donut_delete_Page); 
             module.exports = router; 
